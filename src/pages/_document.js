2678e3c17ccba63d0fbea3ac1c70182905d472aa
Shazaniyu55 <shazaniyu@gmail.com>
2024-04-27 14:34:53 +0100

    SEO META

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Lawrencegebäudereinigung - Home Cleaning Services</title>
+      <meta name="description" content="Lawrencegebäudereinigung offers professional home cleaning services with a focus on people, technologies, and possibilities. Contact us for a sparkling clean home."/>
+      <link rel="Lawrencegebäudereinigung Logo" href="/broom1.png" />
       <body>
         <Main />
         <NextScript />
