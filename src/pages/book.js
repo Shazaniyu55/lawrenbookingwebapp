1e2e29f3d7662e888e67ef5644b3136d38b58b71
Shazaniyu55 <shazaniyu@gmail.com>
2024-05-01 15:52:05 +0100

    john

--- src/pages/book.js
+++ src/pages/book.js
@@ -3,12 +3,25 @@ import Layouts from "../../components/layout";
 import AdainNavBar from "../../components/navbar";
 import Footer from "../../components/footer";
 import { useState } from "react";
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+import { DateRange } from "react-date-range";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css';
 
 function Book(){
 
   const [selectedOption, setSelectedOption] = useState('one-time');
-  const [price, setPrice] = useState(0);
-  const [cleaners , setCleaners] =useState(1)
+  const [price, setPrice] = useState(50);
+  const [counter , setCounter] =useState(1)
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -46,13 +59,25 @@ function Book(){
     // Add logic for form submission (e.g., sending data to server)
   }
 
+  const increment = ()=>{
+    if (counter == 5) {
+        setCounter(counter )
+    }else{
+      var arith = price + counter
+        setCounter(counter + 1)
+        setPrice(arith)
+        
+    }
+    
+  }
+
 
 
         return(
             <>
             <Layouts showNavbar={AdainNavBar} showFooter={Footer}>
                     
-            <div className="grid md:grid-cols-2 gap-10 mt-20 ">
+            <div className="md:grid grid-cols-2 gap-10 mt-20">
             <div className="md:container  bg-yellow m-10 p-4 rounded-md">
             <h1 className="text-3xl md:text-4xl font-bold text-white text-center">BOOK A CLEANING</h1>
 
@@ -62,15 +87,24 @@ function Book(){
 
                 <div className="flex justify-center items-center">
                 <span className="text-center">Lawrencegebäudereinigung</span>
+
                 </div>
 
 
 
-           <div className="md:grid md:grid-cols-3">
+                <div className="flex justify-center mt-10 items-center">
+                <span className="text-center">Packages</span>
+
+                </div>
+
+
+           <div className="md:grid gap-8 md:grid-cols-3">
 
 
         <div className="flex items-center mb-2 mt-10">
-                <input
+
+            <div className="relative container  flex flex-col gap-5 bg-white p-8 rounded-lg">
+            <input
                         type="radio"
                         value="one-time"
                         checked={selectedOption === 'one-time'}
@@ -78,10 +112,23 @@ function Book(){
                         className="mr-2"
                     />
                     <label htmlFor="one-time">One-time Cleaning</label>
-                    </div>
 
 
-            <div className="flex items-center mb-2 mt-10">
+
+                    <p>
+                        On-demand cleaning service<br/>
+                        <span>PRICE PER CLEANER</span>
+                    </p>
+
+            </div>
+                
+                    
+            </div>
+
+
+        <div className="flex items-center mb-2 mt-10">
+        <div className="relative container md:w-[400px]  flex flex-col gap-5  bg-white p-8 rounded-lg">
+
           <input
             type="radio"
             value="basic"
@@ -90,12 +137,23 @@ function Book(){
             className="mr-2"
           />
           <label htmlFor="basic">Basic Cleaning</label>
+
+          <p>
+            On-demand cleaning service<br/>
+            <span>PRICE PER CLEANER</span>
+          </p>
+
+          </div>
+
+         
         </div>
 
 
 
 
         <div className="flex items-center mb-2 mt-10">
+        <div className="relative container md:w-[400px]  flex flex-col gap-5  bg-white p-8 rounded-lg">
+
           <input
             type="radio"
             value="regular"
@@ -104,14 +162,125 @@ function Book(){
             className="mr-2"
           />
           <label htmlFor="regular">Regular Cleaning</label>
+
+          <p>
+            On-demand cleaning service<br/>
+            <span>PRICE PER CLEANER</span>
+          </p>
         </div>
 
+        </div>
 
+        {/* selecting number of cleaners */}
+
+        <div className="md:grid gap-8 grid-row-2">
+
+        <span>Select number of cleaners 1X</span>
+
+        {/* button for increment */}
+        <div className="flex justify-start items-end">
+            <button onClick={increment} className="bg-black py-3 px-8 rounded-md outline-none w-40 text-white">+</button>
+        </div>
+
+            
+            {/* button for decrement */}
+        <div className="flex justify-start items-end">
+            <button onClick={increment} className="bg-black py-3 px-8 rounded-md outline-none w-40 text-white">-</button>
+        </div>
+
+          </div>
+
+
+          {/* calender and time div */}
+          <div>
+
+            {/* <DateRange
+                    editableDateInputs={true}
+                    onChange={(item) => setDate([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}
+                    className="date"
+                    minDate={new Date()}
+                  /> */}
+
+            
+
+          </div>
 
 
                    
                     
-                </div>
+        </div>
+
+          {/* check out form */}
+        <div className="mt-10">
+        <form className="w-full max-w-lg">
+  <div className="flex flex-wrap -mx-3 mb-6">
+    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+        First Name
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
+      <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+    </div>
+    <div className="w-full md:w-1/2 px-3">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+        Last Name
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
+    </div>
+  </div>
+  <div className="flex flex-wrap -mx-3 mb-6">
+    <div className="w-full px-3">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+        Password
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************"/>
+      <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+    </div>
+  </div>
+  <div className="flex flex-wrap -mx-3 mb-2">
+    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+        City
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque"/>
+    </div>
+    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+        Email
+      </label>
+      <div className="relative">
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2  leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="Email"/>
+
+       
+      </div>
+    </div>
+   
+
+
+  </div>
+
+  
+  <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+        Phone Number
+      </label>
+      <PhoneInput
+            inputStyle={{width: '100%'}}
+            country={'ng'}
+          />
+    </div>
+</form>
+        </div>
+
+            
+
+          <div className="flex justify-center m-4 p-4 items-center">
+          <button  className="bg-black py-3 px-8 rounded-md outline-none w-40 text-white">Book Cleaning</button>
+
+
+          </div>
 
 
 
@@ -119,8 +288,8 @@ function Book(){
             </div>
 
 
-            <div className="md:container  bg-yellow m-10 p-4 rounded-md">
-            <div className="md:grid md:grid-cols-2">
+            <div className="md:container  bg-yellow m-10 p-4 rounded-md  ">
+            <div className="md:grid md:grid-cols-2 ">
 
                 {/* packages */}
                 <div>
@@ -132,7 +301,7 @@ function Book(){
                 {/* result */}
                 <div>
                     <p>{selectedOption}</p>
-                    <p className="text-lg">{cleaners}</p>
+                    <p className="text-lg">{counter}</p>
 
                     <p className="text-lg">${price}</p>
 
