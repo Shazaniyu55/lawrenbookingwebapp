0668a6973205c2c7d620ddeff995ada1d34e3275
Shazaniyu55 <shazaniyu@gmail.com>
2024-05-02 12:56:15 +0100

    meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
 
       <Head>
       <title>Lawrencegebaudereinigung</title>
-      <meta name="Lawrencegebaudereinigung logo" content="Lawrencegebaudereinigung offers professional home cleaning services with a focus on people, technologies, and possibilities. Contact us for a sparkling clean home."/>
+      <meta name="Lawrencegebaudereinigung logo" content="Lawrencegebaudereinigung offers professional home cleaning services  Contact us for a sparkling clean home."/>
       <link rel="Lawrencegebaudereinigung logo" href="/favicon.ico" />
       </Head>
       
