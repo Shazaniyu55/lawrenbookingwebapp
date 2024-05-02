7fd291aba5ccb9dbbe3eb245e4c2e597b04bb859
Shazaniyu55 <shazaniyu@gmail.com>
2024-05-02 12:16:41 +0100

    optimize

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+     
+
+      <Head>
       <title>Lawrencegebäudereinigung</title>
-      <meta name="Lawrencegebäudereinigung" content="Lawrencegebäudereinigung offers professional home cleaning services with a focus on people, technologies, and possibilities. Contact us for a sparkling clean home."/>
-      <link rel="icon" href="/broom1.png" />
+      <meta name="Lawrencegebäudereinigung logo" content="Lawrencegebäudereinigung offers professional home cleaning services with a focus on people, technologies, and possibilities. Contact us for a sparkling clean home."/>
+      <link rel="Lawrencegebäudereinigung logo" href="/favicon.ico" />
+      </Head>
+      
 
       <body>
         <Main />
